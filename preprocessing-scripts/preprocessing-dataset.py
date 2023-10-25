826e80ad6f1eeed08c58910c06c932bead8f248b
pfransozi <philipehf@gmail.com>
2023-10-25 15:33:28 -0300

    modify script

--- preprocessing-scripts/preprocessing-dataset.py
+++ preprocessing-scripts/preprocessing-dataset.py
@@ -3,60 +3,91 @@ import getopt
 import os
 import pandas as pd
 
-GOODWARE_CLASSIFICATION = 0
-MALWARE_CLASSIFICATION = 1
-DEBUG = False
+# python preprocessing-dataset.py --input-goodware-batch-dir=./andropytool_output/goodware_batch/ --input-malware-batch-dir=./andropytool_output/malware_batch/ --input-goodware-dir=./andropytool_output/goodware_itens/ --input-malware-dir=./andropytool_output/malware_itens --output-dataframe-file=./csv/
+
+GOODWARE_CLASS = 0
+MALWARE_CLASS = 1
+BATCH = 1
+ITENS = 0
 
 
 def debug(msg):
-    if DEBUG:
-        print(msg)
+    print(msg)
 
 
 def preprocessing_dataframes_save_to_cvs(output_cvs, dframes):
-    dfs_concant = pd.DataFrame()
+    dfs_opcodes = pd.DataFrame()
+    dfs_apicalls = pd.DataFrame()
+    dfs_permissions = pd.DataFrame()
 
     for dframe, class_df in dframes:
-        for col_apk in dframe.columns:
-            json_str = dframe[col_apk]['Static_analysis']
-            
-            df_static_analysis = pd.json_normalize(json_str)
-            
-            df_static_analysis.drop(
-                [col for col in df_static_analysis.columns if is_to_drop(col)], axis=1, inplace=True)
-            
-            permissions = df_static_analysis.pop('Permissions')[0]
 
-            for perm in permissions:
-                df_static_analysis[perm] = 1 if perm not in df_static_analysis else 1 + \
-                    df_static_analysis[perm]
+        opcodes = dframe["Opcodes"]
+        apicalls = dframe["API calls"]
+        permission_dict = {
+            permission: 1 for permission in dframe["Permissions"]}
 
-            df_static_analysis['class'] = class_df
-            dfs_concant = pd.concat([dfs_concant, df_static_analysis])
+        dfs_tmp = pd.DataFrame(opcodes, index=[0])
+        dfs_tmp["class"] = class_df
+        dfs_opcodes = pd.concat([dfs_opcodes, dfs_tmp])
 
-    dfs_concant.fillna(0, inplace=True)
-    dfs_concant.reset_index(inplace=True)
-    dfs_concant.drop(['index'], axis=1, inplace=True)
+        dfs_tmp = pd.DataFrame(apicalls, index=[0])
+        dfs_tmp["class"] = class_df
+        dfs_apicalls = pd.concat([dfs_apicalls, dfs_tmp])
 
-    dfs_concant.to_csv(os.path.join(dir, output_cvs))
+        dfs_tmp = pd.DataFrame(permission_dict, index=[0])
+        dfs_tmp["class"] = class_df
+        dfs_permissions = pd.concat([dfs_permissions, dfs_tmp])
+
+    dfs_opcodes.fillna(0, inplace=True)
+    dfs_opcodes.reset_index(drop=True)
+    # dfs_opcodes.drop(['index'], axis=1, inplace=True)
+    dfs_opcodes.to_csv(os.path.join(output_cvs, "opcodes.csv"))
+
+    dfs_apicalls.fillna(0, inplace=True)
+    # dfs_apicalls.drop(['index'], axis=1, inplace=True)
+    dfs_apicalls.reset_index(drop=True)
+    dfs_apicalls.to_csv(os.path.join(output_cvs, "apicalls.csv"))
+
+    dfs_permissions.fillna(0, inplace=True)
+    dfs_permissions.reset_index(drop=True)
+    # dfs_permissions.drop(['index'], axis=1, inplace=True)
+    dfs_permissions.to_csv(os.path.join(output_cvs, "permissions.csv"))
 
 
 def load_json_malware(input_dir):
-    return load_json(input_dir, MALWARE_CLASSIFICATION)
+    return load_json(input_dir, MALWARE_CLASS, ITENS)
 
 
 def load_json_goodware(input_dir):
-    return load_json(input_dir, GOODWARE_CLASSIFICATION)
+    return load_json(input_dir, GOODWARE_CLASS, ITENS)
 
 
-def load_json(input_dir, classification):
+def load_json_malware_batch(input_dir):
+    return load_json(input_dir, MALWARE_CLASS, BATCH)
+
+
+def load_json_goodware_batch(input_dir):
+    return load_json(input_dir, GOODWARE_CLASS, BATCH)
+
+
+def load_json(input_dir, classification, batch):
     dframes = []
+    pd_json_tmp = pd.DataFrame()
 
     with os.scandir(input_dir) as entries:
         for entry in entries:
             if entry.is_file() and entry.name.endswith('json'):
                 pd_json = pd.read_json(entry.path)
-                dframes.append((pd_json, classification))
+
+                if batch == BATCH:
+                    # pd_json_tmp = pd.DataFrame.from_dict(pd_json["0B5146FFAFEA66C64076D3B61C326FA14CE729BE978788C150763D9D66E96735"]["Static_analysis"])
+                    for col_apk in pd_json.columns:
+                        pd_json_tmp = pd_json[col_apk]["Static_analysis"]
+                        dframes.append((pd_json_tmp, classification))
+                else:
+                    dframes.append((pd_json["Static_analysis"].to_frame()[
+                                   "Static_analysis"], classification))
 
     if len(dframes) == 0:
         print(
@@ -74,26 +105,42 @@ def is_to_drop(col_name):
 def get_dirs_from_args(argv):
     input_goodware_dir = ''
     input_malware_dir = ''
+    input_goodware_batch_dir = ''
+    input_malware_batch_dir = ''
     output_cvs = ''
 
+    # # python preprocessing-dataset.py
+    # --input-goodware-batch-dir=./andropytool_output/goodware_batch/
+# --input-malware-batch-dir=./andropytool_output/malware_batch/
+# --input-goodware-dir=./andropytool_output/goodware_itens/
+# --input-malware-dir=./andropytool_output/malware_itens --output-dataframe-file=./csv/
+
     opts, args = getopt.getopt(argv, ['h'], [
-                               "input-goodware-dir=", "input-malware-dir=", "output-dataframe-file="])
+                               "input-goodware-dir=",
+                               "input-malware-dir=",
+                               "input-goodware-batch-dir=",
+                               "input-malware-batch-dir=",
+                               "output-dataframe-file="])
 
     for opt, arg in opts:
         if opt == '-h':
-            print('preprocessing-dataset.py --input-goodware-dir=<dir> --input-malware-dir=<dir> --output-dataframe-file=<file>')
+            print('preprocessing-dataset.py --input-goodware-dir=<dir> --input-malware-dir=<dir> --input-goodware-batch-dir=<dir> --input-malware-batch-dir=<dir> --output-dataframe-file=<file>')
             sys.exit(-1)
         elif opt in ("--input-goodware-dir"):
             input_goodware_dir = arg
         elif opt in ("--input-malware-dir"):
             input_malware_dir = arg
+        elif opt in ("--input-goodware-batch-dir"):
+            input_goodware_batch_dir = arg
+        elif opt in ("--input-malware-batch-dir"):
+            input_malware_batch_dir = arg
         elif opt in ("--output-dataframe-file"):
             output_cvs = arg
 
-    return (input_goodware_dir, input_malware_dir, output_cvs)
+    return (input_goodware_dir, input_malware_dir, input_goodware_batch_dir, input_malware_batch_dir, output_cvs)
 
 
-def validate_dirs(input_malware_dir, input_goodware_dir, output_cvs):
+def validate_dirs(input_malware_dir, input_goodware_dir, input_malware_batch_dir, input_goodware_batch_dir, output_cvs):
 
     error_message = ''
 
@@ -104,38 +151,55 @@ def validate_dirs(input_malware_dir, input_goodware_dir, output_cvs):
         error_message.join(
             f"Invalid goodware directory ({input_goodware_dir})")
 
+    if (not os.path.isdir(input_malware_batch_dir)):
+        error_message.join(f"Invalid malware directory ({input_malware_dir})")
+
+    if (not os.path.isdir(input_goodware_batch_dir)):
+        error_message.join(
+            f"Invalid goodware directory ({input_goodware_dir})")
+
     if (not os.path.isdir(output_cvs)):
         error_message.join(f"Invalid output directory ({output_cvs})")
+
+    return error_message
 
 
 def main(argv):
 
     debug("Starting preprocessing dataframes from json to cvs.")
 
-    input_malware_dir = ''
-    input_goodware_dir = ''
+    input_malware_itens_dir = ''
+    input_goodware_itens_dir = ''
+    input_malware_batch_dir = ''
+    input_goodware_batch_dir = ''
     output_cvs = ''
 
     debug("Validating working directories.")
 
-    input_malware_dir, input_goodware_dir, output_cvs = get_dirs_from_args(
+    input_goodware_itens_dir, input_malware_itens_dir, input_goodware_batch_dir, input_malware_batch_dir, output_cvs = get_dirs_from_args(
         argv)
 
-    debug(
-        f"Directories loaded: malware = {input_malware_dir}; goodware = {input_goodware_dir}; cvs output file = {output_cvs}.")
+    debug("Directories:")
+    debug(f"malware itens dir = {input_malware_itens_dir}")
+    debug(f"goodware itens dir = {input_goodware_itens_dir}")
+    debug(f"malware batch dir = {input_malware_batch_dir}")
+    debug(f"goodware batch dir = {input_goodware_batch_dir}")
+    debug(f"cvs output file = {output_cvs}.")
 
-    try:
-        validate_dirs(input_goodware_dir, input_malware_dir, output_cvs)
+    msg = validate_dirs(input_goodware_itens_dir, input_malware_itens_dir,
+                        input_malware_batch_dir, input_goodware_batch_dir, output_cvs)
+
+    if (len(msg) > 0):
         debug("Error in working directories.")
-    except Exception as error:
-        print(error)
-        sys.exit()
+        debug(msg)
 
     debug("Loading json to dataframes.")
 
     dframes = []
-    dframes = load_json_malware(input_malware_dir)
-    dframes.append(load_json_goodware(input_goodware_dir))
+    dframes = load_json_malware(input_malware_itens_dir)
+    dframes += load_json_goodware(input_goodware_itens_dir)
+    dframes += load_json_malware_batch(input_malware_batch_dir)
+    dframes += load_json_goodware_batch(input_goodware_batch_dir)
 
     debug("Processing dataframes.")
     preprocessing_dataframes_save_to_cvs(output_cvs, dframes)
